92c65acda34fc40a32c527458841e219a55b14e4
Carlos Fuentes <carlos_fuentes@epam.com>
2025-03-25 18:23:02 -0500

    Final tests

---
+++ movie-app/src/tests/Genre.test.js
@@ -0,0 +1,48 @@
+import {render, screen, fireEvent} from "@testing-library/react"
+import  Genre from '../Genre'
+
+describe("Genre component tests", () => {
+    it("renders all genres", () => {
+        render(<Genre initialGenres={["Action", "Comedy", "Drama", "Thriller", "Sci-Fi"]} />);
+        const actionButton = screen.getByText("Action");
+        const comedyButton = screen.getByText("Comedy");
+        const dramaButton = screen.getByText("Drama");
+        const thrillerButton = screen.getByText("Thriller");
+        const scifiButton = screen.getByText("Sci-Fi");
+
+        expect(actionButton).toBeInTheDocument();
+        expect(comedyButton).toBeInTheDocument();
+        expect(dramaButton).toBeInTheDocument();
+        expect(thrillerButton).toBeInTheDocument();
+        expect(scifiButton).toBeInTheDocument();
+    })
+
+    it("selected genre is highlighted", () => {
+        render(<Genre initialGenres={["Action", "Comedy", "Drama", "Thriller", "Sci-Fi"]}
+                      selectedGenre="Thriller"
+                />);
+        const actionButton = screen.getByText("Action");
+        const comedyButton = screen.getByText("Comedy");
+        const dramaButton = screen.getByText("Drama");
+        const thrillerButton = screen.getByText("Thriller");
+        const scifiButton = screen.getByText("Sci-Fi");
+        
+        expect(actionButton).toHaveClass("genre-button");
+        expect(comedyButton).toHaveClass("genre-button");
+        expect(dramaButton).toHaveClass("genre-button");
+        expect(thrillerButton).toHaveClass("genre-button selected");
+        expect(scifiButton).toHaveClass("genre-button");
+    })
+
+    it("onClick is called when button is clicked", () => {
+        window.alert = jest.fn();
+        render(<Genre initialGenres={["Action", "Comedy", "Drama", "Thriller", "Sci-Fi"]}
+                      selectedGenre="Thriller"
+                      onSelect={(value) => {alert(value)}}
+                />);
+        const comedyButton = screen.getByText("Comedy");
+        fireEvent.click(comedyButton);
+
+        expect(window.alert).toHaveBeenCalledWith("Comedy");
+    })    
+})
