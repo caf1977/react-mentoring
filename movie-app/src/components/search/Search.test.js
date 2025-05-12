e89ad06278402e8ea9fd2e9c815fbc8323badb60
Carlos Fuentes <carlos_fuentes@epam.com>
2025-05-12 09:30:35 -0500

    Fixing lint

--- movie-app/src/components/search/Search.test.js
+++ movie-app/src/components/search/Search.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react"
+import {render, screen, fireEvent} from "@testing-library/react"
 import { useNavigate, useSearchParams } from "react-router-dom";
 import Search from "./Search";
 
@@ -12,7 +12,8 @@ describe("Search component tests", () => {
     let navigateMock, searchParamsMock;
 
     beforeEach(() => {
-        navigateMock = jest.spyOn(global, 'fetch').mockResolvedValue({ json: jest.fn() });
+        fetch = jest.fn();
+        navigateMock = jest.fn();
         searchParamsMock = {
             toString: jest.fn().mockReturnValue("test=123"),
         };
@@ -30,14 +31,14 @@ describe("Search component tests", () => {
     it("renders initial query", () => {
         render(<Search initialQuery={"default query"} />);
         const query = screen.getByLabelText("FIND YOUR MOVIE");
-
+        
         expect(query).toHaveValue("default query");
     })
 
     it("onChange is called when Submit button is clicked", () => {
         window.alert = jest.fn();
-        render(<Search onSearch={(value) => { alert(value) }} />);
-
+        render(<Search onSearch={(value) => {alert(value)}}/>);
+        
         const query = screen.getByLabelText("FIND YOUR MOVIE");
         const button = screen.getByText("SEARCH");
         fireEvent.change(query, { target: { value: "Any movie" } });
@@ -48,11 +49,11 @@ describe("Search component tests", () => {
 
     it("onChange is called when Enter key is pressed", () => {
         window.alert = jest.fn();
-        render(<Search onSearch={(value) => { alert(value) }} />);
-
+        render(<Search onSearch={(value) => {alert(value)}}/>);
+        
         const query = screen.getByLabelText("FIND YOUR MOVIE");
         fireEvent.change(query, { target: { value: "Any movie" } });
-        fireEvent.keyDown(query, { key: "Enter", code: "Enter", charCode: 13 });
+        fireEvent.keyDown(query, {key: "Enter", code: "Enter", charCode: 13});
 
         expect(window.alert).toHaveBeenCalledWith("Any movie");
     })
